9c305b1037a99ed39874b2ea68b813b1e068f274
Yann Pereira <yann.srn@gmail.com>
2019-04-28 19:05:24 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/yannp/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/yannp/workshopmongo/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.yannp.workshopmongo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,7 @@ import com.yannp.workshopmongo.domain.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String>{
+
 	
 	
 
