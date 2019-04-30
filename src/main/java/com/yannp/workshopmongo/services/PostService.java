44bd3f7c3cb466e1a538e3e98fa3af86df089760
Yann Pereira <yann.srn@gmail.com>
2019-04-30 20:48:47 -0300

    Obtendo um post por id

---
+++ src/main/java/com/yannp/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.yannp.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yannp.workshopmongo.domain.Post;
+import com.yannp.workshopmongo.repository.PostRepository;
+import com.yannp.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) throws ObjectNotFoundException {
+
+	return repo.findById(id).orElseThrow(()->new ObjectNotFoundException("Usuário não encontrado"));
+
+	}
+}
+
+	
